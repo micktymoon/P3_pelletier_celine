4598c724c2c88e79da1a5fdd0610957ccc325e44
Céline PELLETIER <celine_pelletier@hotmail.fr>
2020-03-29 18:55:35 +0200

    Premier commit du projet 3.

---
+++ classes.py
@@ -0,0 +1,96 @@
+import sys, pygame
+import random
+pygame.init()
+
+# Screen creation:
+screen = pygame.display.set_mode((300, 300))
+
+
+class Player(pygame.sprite.Sprite):
+    """Class player."""
+
+    def __init__(self, departure):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('MacGyver.png').convert()
+        self.image = pygame.transform.scale(self.image, (20, 20))
+        self.rect = self.image.get_rect()
+        self.speed_right = [20, 0]
+        self.speed_left = [-20, 0]
+        self.speed_up = [0, -20]
+        self.speed_down = [0, 20]
+        self.pos = self.rect.move(departure)
+        self.obj1 = False
+        self.obj2 = False
+
+    def move_right(self):
+        """move the player on the right."""
+
+        self.pos = self.pos.move(self.speed_right)
+        if self.pos.right > 280:
+            self.pos.right = 260
+
+    def move_left(self):
+        """move the player on the left."""
+
+        self.pos = self.pos.move(self.speed_left)
+        if self.pos.left < 0:
+            self.pos.left = 40
+
+    def move_up(self):
+        """move the player on the top."""
+
+        self.pos = self.pos.move(self.speed_up)
+        if self.pos.top < 0:
+            self.pos.top = 40
+
+    def move_down(self):
+        """move the player on the bottom."""
+
+        self.pos = self.pos.move(self.speed_down)
+        if self.pos.bottom > 280:
+            self.pos.bottom = 260
+
+    def draw_me(self):
+        """draw player."""
+
+        screen.blit(self.image, self.pos, self.rect)
+
+    def erase_me(self):
+        self.image.fill((0, 0, 0, 0))
+
+
+class Labobject(pygame.sprite.Sprite):
+    """Class for labyrinth objects."""
+
+    def __init__(self, image, list):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image).convert()
+        self.image = pygame.transform.scale(self.image, (20, 20))
+        self.rect = self.image.get_rect()
+        self.pos = random.choice(list)
+
+    def draw_me(self):
+        """draw object on the screen."""
+        return screen.blit(self.image, self.pos, self.rect)
+
+    def erase_me(self):
+        """clears the object on the screen."""
+        self.image.fill((0, 0, 0, 0))
+
+
+class Guardian(pygame.sprite.Sprite):
+    """Class for the labyrinth guardian."""
+    def __init__(self, arrival):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('Gardien.png').convert()
+        self.image = pygame.transform.scale(self.image, (20, 20))
+        self.rect = self.image.get_rect()
+        self.pos = self.rect.move(arrival)
+
+    def draw_me(self):
+        """draw guardian on the screen."""
+        return screen.blit(self.image, self.pos, self.rect)
+
+    def erase_me(self):
+        """clears the guardian on the screen."""
+        self.image.fill((0, 0, 0, 0))
---
+++ main.py
@@ -0,0 +1,126 @@
+import sys, pygame
+import random
+from random import randrange
+from classes import Player
+from classes import Labobject
+from classes import Guardian
+pygame.init()
+
+
+# Screen creation:
+screen = pygame.display.set_mode((300, 300))
+# Picture of the walls:
+wall = pygame.image.load('floor-tiles-20x20.png').convert()
+
+
+# x = 15
+# y = 15
+# labyrinth = [['']*y for _ in range(x)]
+# print(labyrinth)
+labyrinth = [
+    ['m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['D', 'P', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', '', '', '', '', '', 'm'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['m', '', '', '', '', '', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', 'G', 'A'],
+    ['m', '', '', '', '', '', '', '', '', '', '', '', '', '', 'm'],
+    ['m', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm', 'm']]
+
+
+def draw_labyrinth(lab, l_wall, l_none):
+    x = 0
+    for row in lab:
+        y = 0
+        for column in row:
+            if column == 'm':
+                screen.blit(wall, (x*20, y*20), (100, 0, 20, 20))
+                l_wall.append(screen.blit(wall, (x*20, y*20), (100, 0, 20, 20)))
+            if column == '':
+                screen.blit(wall, (x*20, y*20), (380, 0, 20, 20))
+                l_none.append(screen.blit(wall, (x*20, y*20), (380, 0, 20, 20)))
+            if column == 'D':
+                screen.blit(wall, (x*20, y*20), (160, 20, 20, 20))
+            if column == 'A':
+                screen.blit(wall, (x*20, y*20), (160, 20, 20, 20))
+            y += 1
+        x += 1
+
+
+def draw_people(lab, lettre, classe):
+    x = 0
+    for row in lab:
+        y = 0
+        for column in row:
+            if column == lettre:
+                o = classe((x * 20, y * 20))
+                return o
+            y += 1
+        x += 1
+
+
+list_walls = []
+list_none = []
+draw_labyrinth(labyrinth, list_walls, list_none)
+player = draw_people(labyrinth, "P", Player)
+guardian = draw_people(labyrinth, "G", Guardian)
+ether = Labobject('ether.png', list_none)
+needle = Labobject('aiguille.png', list_none)
+
+
+while 1:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        draw_labyrinth(labyrinth, list_walls, list_none)
+        guardian.draw_me()
+        old_posx = player.pos.x
+        old_posy = player.pos.y
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_UP:
+                player.move_up()
+
+            if event.key == pygame.K_DOWN:
+                player.move_down()
+
+            if event.key == pygame.K_RIGHT:
+                player.move_right()
+
+            if event.key == pygame.K_LEFT:
+                player.move_left()
+
+        if player.pos.colliderect(ether.draw_me()):
+            player.obj1 = True
+            ether.erase_me()
+
+        if player.pos.colliderect(needle.draw_me()):
+            player.obj2 = True
+            needle.erase_me()
+
+        if player.pos.collidelist(list_walls) != -1:
+            player.pos.x = old_posx
+            player.pos.y = old_posy
+
+        if player.pos.colliderect(guardian.draw_me()):
+            if player.obj1 is True and player.obj2 is True:
+                print("YOU WIN")
+                guardian.erase_me()
+                player.pos.y = guardian.pos.y + 20
+                sys.exit()
+            else:
+                print("YOU LOOSE")
+                sys.exit()
+        player.draw_me()
+
+    pygame.display.update()
+    pygame.display.flip()
+
